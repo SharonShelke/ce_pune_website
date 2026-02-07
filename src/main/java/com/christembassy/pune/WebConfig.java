88f4fb6bad96faa96395287806fa35fe82bbaa32
sharon <sharonshelke1@gmail.com>
2026-02-07 08:25:55 +0530

    changes

--- src/main/java/com/christembassy/pune/WebConfig.java
+++ src/main/java/com/christembassy/pune/WebConfig.java
@@ -7,14 +7,14 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 public class WebConfig implements WebMvcConfigurer {
 
 
-    @Value("${frontend.url:http://localhost:3000}")
+    @Value("${frontend.url:http://localhost:3007}")
     private String frontendUrl;
 
 
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
-                .allowedOrigins(frontendUrl, "https://christ-embassy-pune-frontend.vercel.app/", "http://localhost:3000")
+                .allowedOrigins(frontendUrl, "https://christ-embassy-pune-frontend.vercel.app/", "http://localhost:3007")
                 .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
                 .allowedHeaders("*")
                 .allowCredentials(true);
