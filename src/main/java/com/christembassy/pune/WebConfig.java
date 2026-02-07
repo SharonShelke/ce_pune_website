7a91bf587d80500a8eb3f6448a5dc90e1ef4b0ec
sharon <sharonshelke1@gmail.com>
2026-02-07 08:19:57 +0530

    changes

--- src/main/java/com/christembassy/pune/WebConfig.java
+++ src/main/java/com/christembassy/pune/WebConfig.java
@@ -14,7 +14,7 @@ public class WebConfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
-                .allowedOrigins(frontendUrl, "https://christ-embassy-pune-frontend.vercel.app", "http://localhost:3000")
+                .allowedOrigins(frontendUrl, "https://christ-embassy-pune-frontend.vercel.app/", "http://localhost:3000")
                 .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
                 .allowedHeaders("*")
                 .allowCredentials(true);
