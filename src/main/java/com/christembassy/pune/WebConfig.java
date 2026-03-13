44fabd60d421f07d1bfc9a0199d35a4ca28f2149
sharonshelke1 <sharonshelke1@gmail.com>
2026-03-13 08:46:40 +0530

    changes for application.properties

--- src/main/java/com/christembassy/pune/WebConfig.java
+++ src/main/java/com/christembassy/pune/WebConfig.java
@@ -14,7 +14,7 @@ public class WebConfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
-                .allowedOrigins(frontendUrl,"http://localhost:3001")
+                .allowedOrigins(frontendUrl,"http://15.206.166.139")
                 .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
                 .allowedHeaders("*")
                 .allowCredentials(true);
