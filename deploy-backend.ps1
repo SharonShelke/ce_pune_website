9e3dc034946ba3e774ecafa4ab9c254a3d52fdac
sharonshelke1 <sharonshelke1@gmail.com>
2026-07-23 12:43:01 +0530

    changes as Ip has changes

--- deploy-backend.ps1
+++ deploy-backend.ps1
@@ -16,7 +16,11 @@ Write-Host "2. Stopping remote backend service..." -ForegroundColor Green
 & "C:\Windows\System32\OpenSSH\ssh.exe" -o StrictHostKeyChecking=no -i $keyPath $serverHost "sudo systemctl stop ce-pune-backend; sudo pkill -9 -f java"
 
 Write-Host "3. Uploading new JAR to server..." -ForegroundColor Green
-& "C:\Windows\System32\OpenSSH\scp.exe" -o StrictHostKeyChecking=no -i $keyPath $localJar "${serverHost}:/home/ec2-user/"
+& "C:\Windows\System32\OpenSSH\scp.exe" -o StrictHostKeyChecking=no -i $keyPath $localJar "${serverHost}:~/"
+if ($LASTEXITCODE -ne 0) {
+    Write-Error "SCP upload failed!"
+    exit 1
+}
 
 Write-Host "4. Restarting remote backend service..." -ForegroundColor Green
 & "C:\Windows\System32\OpenSSH\ssh.exe" -o StrictHostKeyChecking=no -i $keyPath $serverHost "sudo systemctl start ce-pune-backend"
